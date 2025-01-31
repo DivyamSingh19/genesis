e7e83cbe78bd2abde1fa1ef56500cd3647a369ee
Divyam Singh <divyamsingh1918@gmail.com>
2025-01-31 23:54:33 +0530

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
  
 import './App.css'
-import { BrowserRouter,Routes ,Route } from 'react-router-dom'
+import { Routes ,Route } from 'react-router-dom'
+import Signup from './Pages/Signup'
+import Login from './Pages/Login'
+import GenerationPage from './Pages/GenerationPage'
+import Pricing from './Pages/Pricing'
+import Home from './Pages/Home'
 function App() {
  
 
   return (
     <div>
-       
+       <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/generation' element={<GenerationPage/>}/>
+        <Route path='/pricing' element={<Pricing/>}/>
+       </Routes>
     </div>
   )
 }
