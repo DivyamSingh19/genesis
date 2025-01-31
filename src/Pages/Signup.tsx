ea53b26e1be359df3b07f1fa22f5fb6bf31c3c20
Divyam Singh <divyamsingh1918@gmail.com>
2025-01-31 23:54:25 +0530

    Updates

--- src/Pages/Signup.tsx
+++ src/Pages/Signup.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>
-      
+    <div className='flex items-center justify-center text-white'>
+      signup
     </div>
   )
 }
