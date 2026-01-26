9c51c10fa28ef4f19b806099eb5b3f3fcf58d0b1
Divyam Singh <divyamsingh1918@gmail.com>
2026-01-26 11:11:20 +0530

    updates

--- server/services/chat.service.ts
+++ server/services/chat.service.ts
@@ -3,9 +3,19 @@ import { ServerError } from "./error.service";
 export class ChatService{
     async getChats(userId:string) {
         try {
-            
+            const chats = await prisma.chat.findMany({where:{userId:userId}})
+            return chats
         } catch (error) {
             throw new ServerError("Server failed while fetching chats")
+        }
+    }
+
+    async getChatById(chatId:string){
+        try {
+            const chatdata = await prisma.chat.findFirst({where:{id:chatId}})
+            return chatdata
+        } catch (error) {
+            throw new ServerError("Server failed by fetching chat")
         }
     }
 }
